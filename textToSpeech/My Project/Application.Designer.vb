2d94dd091ddf89a5c46f9a9066b5dac72f783f6c
Whitchurch Muthumani <whitchurch85@yahoo.com>
2020-09-15 14:09:56 -0700

    Commit and push the renamed file

--- textToSpeech/My Project/Application.Designer.vb
+++ textToSpeech/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.textToSpeech.Form1
+            Me.MainForm = Global.textToSpeech.textToSpeechUI
         End Sub
     End Class
 End Namespace
---
+++ textToSpeech/textToSpeechUI.vb
@@ -0,0 +1,3 @@
+﻿Public Class textToSpeechUI
+
+End Class
