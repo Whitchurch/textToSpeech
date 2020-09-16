8acb1684395d14bd0827a19a982934dd8227959d
Whitchurch Muthumani <whitchurch85@yahoo.com>
2020-09-15 19:09:18 -0700

    Commit with voice options

--- textToSpeech/textToSpeechUI.Designer.vb
+++ textToSpeech/textToSpeechUI.Designer.vb
@@ -31,13 +31,18 @@ Partial Class textToSpeechUI
         Me.TrackBar2 = New System.Windows.Forms.TrackBar()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
         CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(85, 185)
+        Me.Button1.Location = New System.Drawing.Point(85, 193)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(230, 66)
         Me.Button1.TabIndex = 0
@@ -46,9 +51,10 @@ Partial Class textToSpeechUI
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(85, 68)
+        Me.TextBox1.Location = New System.Drawing.Point(85, 75)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
         Me.TextBox1.Size = New System.Drawing.Size(668, 111)
         Me.TextBox1.TabIndex = 1
         '
@@ -97,7 +103,7 @@ Partial Class textToSpeechUI
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(334, 185)
+        Me.Button2.Location = New System.Drawing.Point(334, 192)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(204, 66)
         Me.Button2.TabIndex = 7
@@ -106,18 +112,72 @@ Partial Class textToSpeechUI
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(556, 185)
+        Me.Button3.Location = New System.Drawing.Point(556, 191)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(197, 66)
         Me.Button3.TabIndex = 8
         Me.Button3.Text = "Resume"
         Me.Button3.UseVisualStyleBackColor = True
         '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Enabled = False
+        Me.Label4.Location = New System.Drawing.Point(85, 56)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 17)
+        Me.Label4.TabIndex = 9
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.HorizontalScrollbar = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(384, 317)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.ScrollAlwaysVisible = True
+        Me.ListBox1.Size = New System.Drawing.Size(166, 84)
+        Me.ListBox1.TabIndex = 10
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.HorizontalScrollbar = True
+        Me.ListBox2.ItemHeight = 16
+        Me.ListBox2.Location = New System.Drawing.Point(572, 317)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.ScrollAlwaysVisible = True
+        Me.ListBox2.Size = New System.Drawing.Size(181, 84)
+        Me.ListBox2.TabIndex = 11
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(384, 287)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(98, 17)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Usable Voices"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(556, 287)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(109, 17)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Disabled Voices"
+        '
         'textToSpeechUI
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.TrackBar2)
@@ -145,4 +205,9 @@ Partial Class textToSpeechUI
     Friend WithEvents TrackBar2 As TrackBar
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
 End Class
--- textToSpeech/textToSpeechUI.vb
+++ textToSpeech/textToSpeechUI.vb
@@ -4,9 +4,21 @@
 
 
     Private Sub textToSpeechUI_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label4.Text = "The number of voices installed is " & SAPIObject.getvoices.count + 1
         TrackBar1.Value = 0
         TrackBar2.Value = 70
         SAPIObject.Volume = TrackBar2.Value
+
+        For i = 0 To SAPIObject.getvoices.count
+            Try
+                SAPIObject.Voice = SAPIObject.getvoices.item(i)
+                SAPIObject.Speak("")
+                ListBox1.Items.Add("Voice #" & i & " " & SAPIObject.Voice.GetDescription)
+            Catch ex As Exception
+                ListBox2.Items.Add("Voice #" & i & " " & SAPIObject.Voice.GetDescription)
+            End Try
+        Next
+
 
     End Sub
 
@@ -28,5 +40,21 @@
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         SAPIObject.Resume
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        SAPIObject.Voice = SAPIObject.getvoices.Item(Microsoft.VisualBasic.Val(ListBox1.SelectedItem.Substring(7, 2)))
+        SAPIObject.Speak("You have selected " & ListBox1.SelectedItem.ToString)
+    End Sub
+
+    Private Sub ListBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox2.SelectedIndexChanged
+        'SAPIObject.Voice = SAPIObject.getvoices.Item(Microsoft.VisualBasic.Val(ListBox2.SelectedItem.Substring(7, 2)))
+        ListBox2.ClearSelected()
+        SAPIObject.Speak("You have selected a voice that is disabled")
+
     End Sub
 End Class
